487eb4cd4727bed0c42d364a1e259073f6abdc0f
Miguel Pedraza <mikeped469@gmail.com>
2024-01-11 13:45:22 -0600

    Intial commit including installation of react redux toolkit and tailwind plus basic configuration of styles and assets

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// React imports
+import React from "react";
+
+// React dom imports
+import ReactDOM from "react-dom/client";
+
+// Components
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
