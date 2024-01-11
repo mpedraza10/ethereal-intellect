d61d0f03f0991cf3bc81538d460bc1ec007a132d
Miguel Pedraza <mikeped469@gmail.com>
2024-01-11 15:13:29 -0600

    Setting handle change and submit function with just alert

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,13 +5,27 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+	// State
+	const [article, setArticle] = useState({
+		url: "",
+		summary: "",
+	});
+
+	// Helper functions
+
+	// Function to handle the submit of the form
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		alert("Submited!");
+	};
+
 	return (
 		<section className="mt-16 w-full max-w-xl">
 			{/* Search */}
 			<div className="flex flex-col w-full gap-2">
 				<form
 					className="relative flex justify-center items-center"
-					onSubmit={() => {}}
+					onSubmit={handleSubmit}
 				>
 					<img
 						src={linkIcon}
@@ -22,8 +36,8 @@ const Demo = () => {
 						className="url_input peer"
 						type="url"
 						placeholder="Enter a url"
-						value=""
-						onChange={() => {}}
+						value={article.url}
+						onChange={(e) => setArticle({ ...article, url: e.target.value })}
 						required
 					/>
 					<button
